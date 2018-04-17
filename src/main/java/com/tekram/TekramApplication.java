c10ce464ce76982eb21451f7199622e918593987
penanBossia <sibossia@gmail.com>
2018-04-17 14:22:00 +0200

    initial commit

---
+++ src/main/java/com/tekram/TekramApplication.java
@@ -0,0 +1,12 @@
+package com.tekram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TekramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TekramApplication.class, args);
+	}
+}
